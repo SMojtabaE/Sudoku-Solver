93e2f61ff203a25ff6ab223418fad2518cf78861
Seyed Mojtaba Esmaili <mo93esm@gmail.com>
2022-06-24 19:47:44 +0430

    backtracking func added

--- Sudoku_solver.py
+++ Sudoku_solver.py
@@ -1,7 +1,13 @@
 
 # TODO:
-#     -printbored function
-#     -find empty function
+#     -printbored function      DONE
+#     -find empty function      DONE
+#     -check validation         DONE
+#     -Backtracking             DONE
+#     -forward checking
+#     -mrv
+#     -degreee
+
 
 def printboard(board):
 
@@ -16,6 +22,7 @@ def find_empty(board):
         for j in range(len(board)):
             if board[i][j] == 0:
                 return (i,j) # (row,col)
+    return None
 
 
 def number_Of_squers(number): # this function is just for checking that the squer is 2*2 or 3*3 
@@ -24,7 +31,7 @@ def number_Of_squers(number): # this function is just for checking that the sque
     elif number == 4:
         return 2
 
-def check_validation(pos,valu):
+def check_validation(board,pos,valu):
     #checking rows
     rows = board[pos[0]]
     if valu in rows:
@@ -39,23 +46,69 @@ def check_validation(pos,valu):
     starting_row = (pos[0] // squers) * squers
     starting_col = (pos[1] // squers) * squers
 
+    #checking the squer
     for i in range(starting_row,starting_row + squers):
         for j in range(starting_col,starting_col + squers): 
             if board[i][j] == valu:
                 return False
     return True
 
+def solve(board):
+    pos = find_empty(board)
+    if not pos:
+        return True
+    
+    for i in range(1,size_Of_board+1):
+        if check_validation(board,pos,i):
+            board[pos[0]][pos[1]] = i
 
-board = [[0,1,0,4],
-         [3,0,1,0],
-         [0,2,0,1],
-         [0,3,0,0]]
+            if solve(board):
+                return True
+
+            board[pos[0]][pos[1]] = 0
+
+    return False  # this sudoku cant be solved
+
+
+board = [                   #this board cant be solved
+            [0, 1,  0, 4],         
+            [3, 0,  1, 0],
+
+            [0, 2,  0, 1],
+            [0, 3,  0, 0]
+        ]
+
+board1 = [                   #this board cant be solved
+            [3, 4,  1, 0],         
+            [0, 2,  0, 0],
+            
+            [0, 0,  2, 0],
+            [0, 1,  4, 3]
+        ]
+
+board2 = [
+            [3, 9, 0,   0, 5, 0,   0, 0, 0],
+            [0, 0, 0,   2, 0, 0,   0, 0, 5],
+            [0, 0, 0,   7, 1, 9,   0, 8, 0],
+
+            [0, 5, 0,   0, 6, 8,   0, 0, 0],
+            [2, 0, 6,   0, 0, 3,   0, 0, 0],
+            [0, 0, 0,   0, 0, 0,   0, 0, 4],
+
+            [5, 0, 0,   0, 0, 0,   0, 0, 0],
+            [6, 7, 0,   0, 0, 5,   0, 4, 0],
+            [0, 0, 9,   0, 0, 0,   2, 0, 0]
+        ]
 
 size_Of_board =  int(input("enter the size of sudoku : "))
 squers = number_Of_squers(size_Of_board)
-# print(squers)
 
-# printboard(board)
 
-# for i in range(len(board)):
-#     print(check_validation((0,0),i+1))
+printboard(board1)
+
+if solve(board1):
+    print("-----Solved-Sudoku-----")
+    printboard(board1)
+else:
+    print("board cant be solved")
+
